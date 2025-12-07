d29affe61225505ed17f6101671a97bbb1654518
Joko <jokoadhikusumo@gmail.com>
2025-12-07 17:52:56 +0700

    update logo

--- script.js
+++ script.js
@@ -1038,7 +1038,20 @@ auth.onAuthStateChanged(async (user) => {
     if (mainBanner) mainBanner.classList.remove("hidden");
 
     // --- TAMPILAN HEADER (Masuk) ---
-    authBtn.textContent = "Masuk";
+    // Menggunakan display: inline-block; style inline dan menghapus whitespace.
+    authBtn.innerHTML = `
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="currentColor" 
+            class="h-4 w-4 sm:h-5 sm:w-5 bi bi-person-circle mr-1 flex-shrink-0" 
+            viewBox="0 0 16 16"
+            style="display: inline-block; vertical-align: middle;" 
+        >  
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>  
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg>Masuk
+    `;
+
     authBtn.classList.remove("bg-red-500", "text-white", "hover:bg-red-600");
     authBtn.classList.add("bg-gold-accent", "text-navy-blue", "hover:bg-white");
 
